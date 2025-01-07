899a0b5b0fe0434849d243e5881fb5c85b18a73f
hwnginsoict <thoquyenv@gmail.com>
2025-01-07 17:59:03 +0700

    add

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from graph.graph import Graph
 from problem import Problem
 
 if __name__ == "__main__":
-    filepath = 'F:\\CodingEnvironment\\DPDPTW2F\\data\\dpdptw\\200\\LC1_2_1.csv'
+    filepath = '.\\data\\dpdptw\\200\\LC1_2_1.csv'
     graph = Graph(filepath)
     problem = Problem(filepath)
     ga_lerk = GA_LERK(graph, problem, 10, 100, 50)
