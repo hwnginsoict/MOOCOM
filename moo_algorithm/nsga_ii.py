0ee21100fe9a796e9b0aab6b3cc0aad8f3878aab
hwnginsoict <thoquyenv@gmail.com>
2025-06-02 16:08:38 +0700

    add

--- moo_algorithm/nsga_ii.py
+++ moo_algorithm/nsga_ii.py
@@ -153,7 +153,7 @@ def run_nsga_ii(processing_number, problem, indi_list, pop_size, max_gen, crosso
     
 
 if __name__ == "__main__":
-    from util_bi_tsp import GetData, crossover, mutation, tour_cost, create_individual
+    from util_tri_tsp import GetData, crossover, mutation, tour_cost, create_individual
 
     num = 20
 
