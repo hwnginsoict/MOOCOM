38984ae2afab6bebe5139f0625ac66e7408e9de4
hwnginsoict <thoquyenv@gmail.com>
2025-06-18 21:32:55 +0700

    add

--- moo_algorithm/nsga_ii_cvrp.py
+++ moo_algorithm/nsga_ii_cvrp.py
@@ -158,11 +158,11 @@ if __name__ == "__main__":
     from util_bi_cvrp import GetData, crossover, mutation, tour_cost, create_individual
 
     num = 8
-    size = 20
+    size = 50
 
-    ref_point = np.array([30, 8])
+    ref_point = np.array([45, 8])
 
-    print("bi cvro 20")
+    print("bi cvrp 50")
     print(ref_point)
 
     data = GetData(num,size)
