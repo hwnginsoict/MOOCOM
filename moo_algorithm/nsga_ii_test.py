82f71e5452e58df8609fb70b4503685387a5e488
hwnginsoict <thoquyenv@gmail.com>
2025-06-19 14:52:46 +0700

    add

--- moo_algorithm/nsga_ii_test.py
+++ moo_algorithm/nsga_ii_test.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     from util_bi_kp import GetData, crossover, mutation, tour_cost, create_individual
 
     num = 8
-    size = 50
+    size = 100
 
     if size == 50:
         ref_point = np.array([-5, -5])
